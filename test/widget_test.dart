6453f0d952c91390eab93102d6c51ed97c6d144c
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2026-03-29 11:34:57 -0300

    chore: configurar riverpod como gerenciamento de estado e DI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:senior_ease/main.dart';
 
 void main() {
   testWidgets('App renders SeniorEase text', (WidgetTester tester) async {
-    await tester.pumpWidget(const SeniorEaseApp());
+    await tester.pumpWidget(const ProviderScope(child: SeniorEaseApp()));
 
     expect(find.text('SeniorEase'), findsOneWidget);
   });
